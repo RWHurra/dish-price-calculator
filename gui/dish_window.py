a7838828800aef859ec4ab635203c0e8f3f23cf6
Robert <robbas@gmail.com>
2023-07-12 23:27:10 +0200

    Refactored dish window
    Change the code to smaller understandable functions.
    
    Changed some texts:
    - in create new dish, the unit is now with quantity
    - in dish table, after each component name there is now ":"

--- gui/dish_window.py
+++ gui/dish_window.py
@@ -28,63 +28,24 @@ class DishWindow(QMainWindow):
         self.component_instance = Component()
 
         # Create layout for the left side (dishes table and delete button)
-        left_layout = QVBoxLayout()
-        main_layout.addLayout(left_layout)
+        self.left_layout = QVBoxLayout()
+        main_layout.addLayout(self.left_layout)
 
         # Create table widget for displaying dishes
-        self.table_widget = QTableWidget()
-        self.table_widget.setColumnCount(3)
-        self.table_widget.setHorizontalHeaderLabels(["Name", "Total Price", "Dish Components"])
-        left_layout.addWidget(self.table_widget)
+        self.create_dish_table_widget()
 
         # Create button for deleting a dish
-        delete_button = QPushButton("🗑️ Delete Dish")
-        delete_button.clicked.connect(self.delete_dish)
-        left_layout.addWidget(delete_button)
+        self.create_delete_dish_button()
 
         # Create layout for the right side (GUI to create a new dish)
-        right_layout = QVBoxLayout()
-        main_layout.addLayout(right_layout)
+        self.right_layout = QVBoxLayout()
+        main_layout.addLayout(self.right_layout)
 
         # Create widget for adding a new dish
-        add_widget = QWidget()
-        add_layout = QVBoxLayout()
-        add_widget.setLayout(add_layout)
-
-        name_label = QLabel("Name:")
-        self.name_input = QLineEdit()
-        add_layout.addWidget(name_label)
-        add_layout.addWidget(self.name_input)
+        self.create_new_dish_widget()
 
         # Create table widget for listing assigned dish components
-        self.components_table = QTableWidget()
-        self.components_table.setColumnCount(2)
-        self.components_table.setHorizontalHeaderLabels(["Dish Component", "Quantity"])
-        add_layout.addWidget(self.components_table)
-
-        component_layout = QHBoxLayout()
-        component_label = QLabel("Dish Component:")
-        self.component_combo = QComboBox()
-        try:
-            self.component_combo.addItems(self.component_instance.load_components())
-        except:
-            self.component_combo.addItems(['No components found'])
-        quantity_label = QLabel("Quantity:")
-        self.quantity_input = QLineEdit()
-        add_component_button = QPushButton("➕ Add Dish Component")
-        add_component_button.clicked.connect(self.add_component)
-        component_layout.addWidget(component_label)
-        component_layout.addWidget(self.component_combo)
-        component_layout.addWidget(quantity_label)
-        component_layout.addWidget(self.quantity_input)
-        component_layout.addWidget(add_component_button)
-        add_layout.addLayout(component_layout)
-
-        save_button = QPushButton("💾 Save Dish")
-        save_button.clicked.connect(self.save_dish)
-        add_layout.addWidget(save_button)
-
-        right_layout.addWidget(add_widget)
+        self.create_assigned_dish_components_table_widget()
 
         self.setCentralWidget(main_widget)
 
@@ -101,11 +62,62 @@ class DishWindow(QMainWindow):
             self.table_widget.setItem(row, 1, total_price_item)
             self.table_widget.setItem(row, 2, components_item)
     
+    def create_dish_table_widget(self):
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(3)
+        self.table_widget.setHorizontalHeaderLabels(["Name", "Total Price", "Dish Components"])
+        self.left_layout.addWidget(self.table_widget)
+
+    def create_delete_dish_button(self):
+        delete_button = QPushButton("🗑️ Delete Dish")
+        delete_button.clicked.connect(self.delete_dish)
+        self.left_layout.addWidget(delete_button)
+
+    def create_new_dish_widget(self):
+        self.add_widget = QWidget()
+        self.add_layout = QVBoxLayout()
+        self.add_widget.setLayout(self.add_layout)
+
+        name_label = QLabel("Name:")
+        self.name_input = QLineEdit()
+        self.add_layout.addWidget(name_label)
+        self.add_layout.addWidget(self.name_input)
+
+    def create_assigned_dish_components_table_widget(self):
+        self.components_table = QTableWidget()
+        self.components_table.setColumnCount(2)
+        self.components_table.setHorizontalHeaderLabels(["Dish Component", "Quantity"])
+        self.add_layout.addWidget(self.components_table)
+
+        component_layout = QHBoxLayout()
+        component_label = QLabel("Dish Component:")
+        self.component_combo = QComboBox()
+        try:
+            self.component_combo.addItems(self.component_instance.load_components())
+        except:
+            self.component_combo.addItems(['No components found'])
+        quantity_label = QLabel("Quantity:")
+        self.quantity_input = QLineEdit()
+        add_component_button = QPushButton("➕ Add Component to dish")
+        add_component_button.clicked.connect(self.add_component)
+        component_layout.addWidget(component_label)
+        component_layout.addWidget(self.component_combo)
+        component_layout.addWidget(quantity_label)
+        component_layout.addWidget(self.quantity_input)
+        component_layout.addWidget(add_component_button)
+        self.add_layout.addLayout(component_layout)
+
+        save_button = QPushButton("💾 Create Dish")
+        save_button.clicked.connect(self.save_dish)
+        self.add_layout.addWidget(save_button)
+
+        self.right_layout.addWidget(self.add_widget)
+
     def get_component_details(self, dish):
         # I have no idea how the line below works, got it from ChatGPT
         # but it lists the components of the dish in the following format:
         # <component_name> <component_quantity> <component_unit>
-        components_with_quantity_and_unit = (", ".join([f"{component['component']} {component['quantity']} {next(item['unit'] for item in self.component_instance.get_components() if item['name'] == component['component'])}" for component in dish['components']]))
+        components_with_quantity_and_unit = (", ".join([f"{component['component']}: {component['quantity']} {next(item['unit'] for item in self.component_instance.get_components() if item['name'] == component['component'])}" for component in dish['components']]))
         return components_with_quantity_and_unit
 
     def list_dish_components(self, components_list):
@@ -113,7 +125,12 @@ class DishWindow(QMainWindow):
         return components
 
     def add_component(self):
-        component = self.component_combo.currentText()
+        component = self.component_combo.currentText().split(" (")[0] # The split removes unit from string
+        unit = ""
+        for component_source in self.component_instance.get_components():
+            if component_source['name'] == component:
+                unit = component_source['unit']
+
         quantity = self.quantity_input.text()
         
         # Perform any necessary validation on input
@@ -122,7 +139,7 @@ class DishWindow(QMainWindow):
         row_count = self.components_table.rowCount()
         self.components_table.setRowCount(row_count + 1)
         self.components_table.setItem(row_count, 0, QTableWidgetItem(component))
-        self.components_table.setItem(row_count, 1, QTableWidgetItem(quantity))
+        self.components_table.setItem(row_count, 1, QTableWidgetItem(quantity + " (" + unit + ")"))
         
         # Clear the component and quantity input fields
         self.quantity_input.clear()
@@ -134,8 +151,8 @@ class DishWindow(QMainWindow):
         component_rows = self.components_table.rowCount()
         components = []
         for row in range(component_rows):
-            component = self.components_table.item(row, 0).text().split(" (")[0] # The split removes unit from string
-            quantity = self.components_table.item(row, 1).text()
+            component = self.components_table.item(row, 0).text()
+            quantity = self.components_table.item(row, 1).text().split(" (")[0] # The split removes unit from string
             components.append({"component": component, "quantity": quantity})
         
         # Perform any necessary validation on input
